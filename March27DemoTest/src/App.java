a9ded1d10ac811b2ca237b5942e9414b2787d692
Webster Fox <w.fox@rcsd.ca>
2024-03-27 11:18:13 -0600

    Demo Repository Test Project

---
+++ March27DemoTest/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Classs!");
+        System.out.println("This is a test that you successfully cloned the demo repository to your desktop");
+    }
+}
