50fac308102d3b295f5066ef6b6d9ab2a866abdb
Webster Fox <w.fox@rcsd.ca>
2024-04-08 11:27:30 -0600

    Classes and Conditionals Demos

---
+++ April8ConditionalDemo/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // int x = -13;
+
+        // // This condition checks if x over 5
+        // if (x>5) {
+        //     System.out.println("BIG NUMBER");
+        // }
+
+        // // This is -12 < x < 5 because 5 already checked
+        // else if (x > -12) {
+        //     System.out.println("Mild Weather");
+        // }
+
+        // // This is x =< -12 becasue the others have been checked
+        // else {
+        //     System.out.println("VERY COLD");
+        // }
+
+
+
+        boolean isCold = false;
+
+        if (isCold) {
+            System.out.println("Wear a coat");
+            System.out.println("Wear Gloves");
+        }
+    }
+}
