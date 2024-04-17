2b29bcbb164193641d4a3ace0aea79ccd73130da
Webster Fox <w.fox@rcsd.ca>
2024-04-17 11:27:18 -0600

    Added Geometry Demo

---
+++ GeometryDemo/src/Geometry.java
@@ -0,0 +1,20 @@
+public class Geometry {
+
+    // Perimeter of a rectangle
+    public void perimeter(double len, double wid) {
+
+        System.out.println("Print your answer here after calculating");
+    }
+
+    // Surface area of a cube
+    public void surfaceArea(double side) {
+
+        System.out.println("Print your answer here after calculating");
+    }
+
+    // Area of a circle
+    public void circleArea(double radius) {
+        
+        System.out.println("Print your answer here after calculating");
+    }
+}
---
+++ GeometryDemo/src/GeometryRunner.java
@@ -0,0 +1,12 @@
+// Name: 
+// Date: 
+// Program Name: 
+// Program Description: 
+//
+
+public class GeometryRunner {
+    public static void main(String[] args) throws Exception {
+        //Import your geometry class, then design your control code
+
+    }
+}
