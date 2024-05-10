10711aa87bffb4685f9c3f3825cfca93009cf987
Webster Fox <w.fox@rcsd.ca>
2024-05-10 11:37:21 -0600

    ArraysLesson-part1

---
+++ ArraysLesson/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // // Declaring Arrays
+        // int[] highscore = null;
+        // String[] names = null;
+
+        // // Create the array
+        // highscore = new int[5];
+        // names = new String[5];
+
+        // // Set initial values invidiually
+        // highscore[0] = 99;
+        // names[0] = "george";
+
+        // Declare, Create, and intialize at the same time
+        int[] highscore = {99,98,98,88,68};
+        String[] names = {"g","fred","wolf","fox","sutter"};
+
+        System.out.println("The length is: " + names.length);
+    }
+}
